8fbe9e4e0eb38a09d9ea079d67e71d9eb5d4621a
Euibin Cheon <euibinc@gmail.com>
2016-10-11 10:50:36 -0400

    Update browse events component

--- frontend/components/browse_events/browse_events.jsx
+++ frontend/components/browse_events/browse_events.jsx
@@ -5,9 +5,24 @@ export default class BrowseEvents extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    this.props.requestEvents();
+  }
+
+  componentWillMount() {
+  }
+
+  // To be added when adding filters
+  // componentWillMount() {
+  //   this.props.updateFilter('user_id', this.props.currentUser.id);
+  // }
+
   render() {
     return (
-      <div>HELLO!</div>
+      <div>
+        <h1>Total number of events</h1>
+        <p>{Object.keys(this.props.events).length}</p>
+      </div>
     );
   }
 }
--- frontend/components/browse_events/browse_events_container.js
+++ frontend/components/browse_events/browse_events_container.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import BrowseEvents from './browse_events';
+import { requestEvents, updateFilter } from '../../actions/event_actions.js';
 
 const mapStateToProps = state => {
-
+  return {
+    events: state.events,
+    filters: state.filters
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-
+  return {
+    requestEvents: () => dispatch(requestEvents()),
+    updateFilter: (filter, value) => dispatch(updateFilter(filter, value))
+  };
 };
 
 export default connect(
