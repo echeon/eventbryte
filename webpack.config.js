8f29aae64b251769fa89623cdd850f15619afa34
Euibin Cheon <euibinc@gmail.com>
2016-10-24 17:16:11 -0400

    Update webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -25,17 +25,5 @@ module.exports = {
       }
     ]
   },
-  plugins:[
-    new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress:{
-        warnings: true
-      }
-    })
-  ],
   devtool: 'source-maps'
 };
